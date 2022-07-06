489a1c4ca544fb81b2b4c1df4af1248255558e6e
Fabio Mazza <fab4mazz@gmail.com>
2022-07-06 22:44:30 +0200

    fix bug in rankers run

--- runs/run_rankers_base.py
+++ runs/run_rankers_base.py
@@ -6,9 +6,6 @@ import pandas as pd
 import numpy as np
 
 
-from covasibyl import base, utils
-from covasibyl import ranktest as rktest
-
 import base_sim_run as base_sim
 
 
@@ -18,22 +15,32 @@ OUT_FOLD = "results"
 values_vload = [1.53846, 0.76923]
 
 
-def get_ranker(which, tau, delta, beta, mu):
+def get_ranker(which, tau, delta, beta, mu, seed=1):
     
-    def_t = 10
+    DEF_TAU = 10
     if which == "MF":
         from covasibyl.rankers import mean_field_rank
-        return mean_field_rank.MeanFieldRanker(tau=tau if tau > def_t else def_t,
+        return mean_field_rank.MeanFieldRanker(tau=tau,
+                delta=delta,mu=mu,lamb=beta)
+    elif which =="MFex":
+        from statmf import meanfield
+        return meanfield.MeanFieldRanker(tau=tau,
                 delta=delta,mu=mu,lamb=beta)
     elif which=="CT":
         from covasibyl.rankers import tracing_rank
-        return tracing_rank.TracingRanker(tau=tau if tau > def_t else def_t, lamb=beta)
+        return tracing_rank.TracingRanker(tau=tau if tau > DEF_TAU else DEF_TAU, lamb=beta)
+    elif which=="DCT":
+        from mitirankers import dct_rank
+        return dct_rank.DctRanker(tau=tau, seed=seed)
     elif which=="greedy":
         from covasibyl.rankers import greedy_rank
         return greedy_rank.GreedyRanker(lamb=beta, tau=tau if tau > 5 else greedy_rank.TAU_INF)
     elif which=="greedy2n":
         from covasibyl.rankers import greedy_rank
         return greedy_rank.GreedyRanker(lamb=beta, tau=tau if tau > 5 else greedy_rank.TAU_INF, sec_neigh=True )
+    elif which == "rand" or which =="random":
+        from covasibyl.rankers import random_rank
+        return random_rank.RandomRanker()
     else:
         raise ValueError("Ranker not recognized")
     
@@ -50,14 +57,16 @@ if __name__ == "__main__":
 
     rk_name = args.ranker
 
-    ranker = get_ranker(rk_name, tau=args.tau, delta=3, 
-            beta=base_sim.BETA, mu=MU_SIR)
+    ranker = get_ranker(rk_name, tau=args.tau, delta=args.delta, 
+            beta=base_sim.BETA*base_sim.REL_T_MED, mu=MU_SIR)
 
     interv = base_sim.make_interv_new(ranker, rk_name, args)
 
     if args.full_iso:
         interv.iso_cts_strength = 0.
         args.prefix+="fulliso_"
+    
+    args.prefix+="bmed_"
     #interv.mitigate = False
     #interv._check_epi_tests = True
     #args.prefix +="nomit_rndtest_"
